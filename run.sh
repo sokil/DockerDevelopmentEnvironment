792bf5e8f7e1b8c2066cb595140443fadb212210
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-06-17 00:02:26 +0300

    usability

--- run.sh
+++ run.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR=$(dirname $(readlink -f $0))
-ENVIRONMENT_DIR=${CURRENT_DIR}/env
+PROJECT_FILES_DIR=${CURRENT_DIR}/projects
 
 function print_usage {
-    message=$1
-    echo -e "\033[1;37mUseage\033[0m: ./run.sh [command] [environment_name]"
-    echo $message
+    MESSAGE=$1
+    echo -e "\033[1;37mUseage\033[0m: ./run.sh command project_name [command_argument, ...]"
+    echo $MESSAGE
 }
 
-function print_help {
-    print_usage
+function print_available_commands {
     echo -e "\033[1;37mAvailable commands:\033[0m"
-    echo -e "\033[0;32mbuild\033[0m: build containers"
+    echo -e "\033[0;32mup\033[0m: build or run containers"
     echo -e "\033[0;32mbash\033[0m: launch bash in container"
     echo -e "\033[0;32mshell_php\033[0m: launch bash as www-user in container"
     echo -e "\033[0;32mshell_mysql\033[0m: launch mysql in container"
     echo -e "\033[0;32mdrop\033[0m: drop container"
     echo ""
-    echo -e "\033[1;37mAvailable environments:\033[0m"
-    find $ENVIRONMENT_DIR -type f -printf "%f\n" -name ".env" | sed 's/\.env//g'
+}
+
+function print_available_projects {
+    echo -e "\033[1;37mAvailable projects:\033[0m"
+    find $PROJECT_FILES_DIR -type f -printf "%f\n" -name ".env" | sed 's/\.env//g' | xargs -I{} echo "-" {}
     echo ""
 }
 
@@ -36,75 +38,79 @@ function drop_container {
     done
 
     # drop containers
-    docker ps -a -f NAME=$PROJECT_NAME --format "{{.Names}}" | xargs -I{} docker rm {}
+    docker ps -a -f NAME=${PROJECT_NAME} --format "{{.Names}}" | xargs -I{} docker rm {}
+}
+
+function up_container {
+    PROJECT_NAME=$1
+    echo $IMAGE_NGINX;
 }
 
 function exec_container_command_root {
-    service_name=$1
-    command=$2
-    docker exec -it ${PROJECT_NAME}_${service_name} $command
+    PROJECT_NAME=$1
+    SERVICE_NAME=$2
+    SHELL_COMMAND=$3
+    docker exec -it ${PROJECT_NAME}_${SERVICE_NAME} ${SHELL_COMMAND}
 }
 
 function exec_container_command_user {
-    service_name=$1
-    command=$2
-    user=$3
-    docker exec --user $user -it ${PROJECT_NAME}_${service_name} $command
+    PROJECT_NAME=$1
+    SERVICE_NAME=$2
+    SHELL_COMMAND=$3
+    USER_NAME=$4
+    docker exec --user ${USER_NAME} -it ${PROJECT_NAME}_${SERVICE_NAME} $SHELL_COMMAND
 }
 
+# read project name from cli arguments
+PROJECT_NAME=$1
+if [[ -z $PROJECT_NAME ]];
+then
+    print_usage
+    print_available_commands
+    print_available_projects
+    exit
+fi
+
+# check project file
+PROJECT_FILE=${PROJECT_FILES_DIR}/${PROJECT_NAME}.env
+if [[ ! -e $PROJECT_FILE ]];
+then
+    echo -e "\033[1;31mInvalid project specified\033[0m"
+    print_available_projects
+    exit
+fi
+
 # read command cli arguments
-COMMAND_NAME=$1
+COMMAND_NAME=$2
 if [[ -z $COMMAND_NAME ]];
 then
-    print_usage "Command name not specified"
+    echo -e "\033[1;31mInvalid command specified\033[0m"
+    print_available_commands
     exit
 fi
 
-# if command is help - prent help
-if [[ $COMMAND_NAME = "help" ]];
-then
-    print_help
-    exit
-fi
-
-# read environment name from cli arguments
-ENVIRONMENT_NAME=$2
-if [[ -z $ENVIRONMENT_NAME ]];
-then
-    print_usage "Environment name not specified"
-    exit
-fi
-
-# import environment
-ENVIRONMENT_FILE=${ENVIRONMENT_DIR}/${ENVIRONMENT_NAME}.env
-if [[ ! -e $ENVIRONMENT_FILE ]];
-then
-    echo "Environment file not found"
-    exit
-fi
-
-source $ENVIRONMENT_FILE
+# import project
+source $PROJECT_FILE
 
 # dispatch command
 case $COMMAND_NAME in
-    build)
-        echo "hello1"
+    up)
+        up_container ${PROJECT_NAME}
         ;;
     bash)
-        $service=$3
-        exec_container_command_root $service bash
+        SERVICE_NAME=$3
+        exec_container_command_root ${PROJECT_NAME} $SERVICE_NAME bash
         ;;
     shell_php)
-        exec_container_command_user php bash www-data
+        exec_container_command_user ${PROJECT_NAME} php bash www-data
         ;;
     shell_mysql)
-        exec_container_command_root php "mysql ${PROJECT_NAME}"
+        exec_container_command_root ${PROJECT_NAME} php "mysql ${PROJECT_NAME}"
         ;;
     drop)
-        drop_container ${PROJECT_NAME}"
+        drop_container "${PROJECT_NAME}"
         ;;
     *)
         echo "Unknown command"
         ;;
 esac
-
