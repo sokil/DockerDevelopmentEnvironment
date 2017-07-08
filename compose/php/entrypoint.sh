872bf5313099fd9217efaa2943be412d639d36d5
Sokil <dmytro.sokil@gmail.com>
2017-07-08 18:19:14 +0300

    Update entrypoint.sh

--- compose/php/entrypoint.sh
+++ compose/php/entrypoint.sh
@@ -27,12 +27,12 @@ then
         pdo_mysql \
         opcache
         
-    # gd extensions
+    # gd/exif extensions
     apt-get install --no-install-recommends -y libpng-dev libjpeg-dev libjpeg62-turbo-dev libfreetype6-dev
     docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
-    docker-php-ext-install gd
+    docker-php-ext-install gd exif
       
-    # intl extensions
+    # intl extension
     apt-get install --no-install-recommends -y zlib1g-dev libicu-dev g++
     docker-php-ext-configure intl
     docker-php-ext-install intl
