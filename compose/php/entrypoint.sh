e290e071da1ca0d75a4c169fc3ded74c5dbfd695
Sokil <dmytro.sokil@gmail.com>
2018-11-22 23:09:18 +0200

    zip

--- compose/php/entrypoint.sh
+++ compose/php/entrypoint.sh
@@ -18,7 +18,21 @@ then
     # add common library requirements
     apt-get update
     apt-get install --no-install-recommends -y \
-        libssl-dev
+        libssl-dez
+
+    # zip
+    apt-get install --no-install-recommends -y zlib1g-dev
+    docker-php-ext-install zip
+
+    # gd/exif extensions
+    apt-get install --no-install-recommends -y libpng-dev libjpeg-dev libjpeg62-turbo-dev libfreetype6-dev
+    docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+    docker-php-ext-install gd exif
+      
+    # intl extension
+    apt-get install --no-install-recommends -y libicu-dev g++
+    docker-php-ext-configure intl
+    docker-php-ext-install intl
 
     # common extensions
     docker-php-ext-install \
@@ -29,16 +43,6 @@ then
         pdo_pgsql \
         opcache
         
-    # gd/exif extensions
-    apt-get install --no-install-recommends -y libpng-dev libjpeg-dev libjpeg62-turbo-dev libfreetype6-dev
-    docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
-    docker-php-ext-install gd exif
-      
-    # intl extension
-    apt-get install --no-install-recommends -y zlib1g-dev libicu-dev g++
-    docker-php-ext-configure intl
-    docker-php-ext-install intl
-
     # node
     # curl -sL https://deb.nodesource.com/setup_8.x | bash -
     # apt-get install -y nodejs
